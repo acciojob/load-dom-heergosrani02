b2356a04795d4711c5379fddedc963c15e4850e0
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-28 23:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //your JS code here. If required.
-let domLoad = document.createElement("div");
-domLoad.innerHTML = "DOM load success";
-document.body.appendChild(domLoad);
+document.addEventListener("DOMContentLoaded", function() {
+    let domLoad = document.createElement("div");
+    domLoad.innerHTML = "DOM load success";
+    document.body.appendChild(domLoad);
+});
